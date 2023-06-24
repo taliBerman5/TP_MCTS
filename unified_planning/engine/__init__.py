540fa0a7f47edf6e58f645ceffa62c1683f889ba
taliberman5 <taliberman5@gmail.com>
2023-06-24 12:12:42 +0300

    added relevant

--- unified_planning/engine/__init__.py
+++ unified_planning/engine/__init__.py
@@ -1,7 +1,14 @@
 
 from unified_planning.engine.converted_problem import Converted_problem
-
+from unified_planning.engine.action import (
+    Action,
+    InstantaneousAction,
+    InstantaneousStartAction,
+)
 
 __all__ = [
     "Converted_problem",
+    "Action",
+    "InstantaneousAction",
+    "InstantaneousStartAction",
 ]
