5a836f5dcff3b86996e854f3560f0c24de3f0f72
taliberman5 <taliberman5@gmail.com>
2023-06-28 20:24:08 +0300

    changed count to 1

--- unified_planning/engines/node.py
+++ unified_planning/engines/node.py
@@ -1,10 +1,12 @@
 import unified_planning as up
 from typing import List, Dict
-
+from unified_planning.engines.utils import (
+    update_stn,
+)
 
 class Node:
     def __init__(self):
-        self._count = 1
+        self._count = 0
         self._value = 0.0
 
     def __repr__(self):
@@ -24,15 +26,14 @@ class Node:
         self._count += 1
 
 class SNode(Node):
-    def __init__(self, state: "up.engines.State", depth: int, possible_actions: List["up.engines.Action"], parent: "up.engines.ANode"=None):
+    def __init__(self, state: "up.engines.State", depth: int, possible_actions: List["up.engines.Action"], stn: "up.plans.stn.STNPlan", parent: "up.engines.ANode"=None):
         super().__init__()
         self._state = state
         self._depth = depth
         self._parent = parent
         self._children: Dict["up.engines.Action", "up.engines.ANode"] = {}
         self._possible_actions = possible_actions
-
-        self._add_children()
+        self._add_children(stn)
 
     def __repr__(self):
         s = "state Node; depth: %d; children: %d; visits: %d; reward: %f" % (self.depth, len(self.children), self.count, self.value)
@@ -62,20 +63,22 @@ class SNode(Node):
         if action in self._possible_actions:
             self._possible_actions.remove(action)
 
-    def _add_children(self):
+    def _add_children(self, stn: "up.plans.stn.STNPlan"):
         for action in self.possible_actions:
-            child = ANode(action, self)
+            child = ANode(action, stn.clone(), self)
             self.children[action] = child
 
 
 
 class ANode(Node):
-    def __init__(self, action: "up.engines.action.Action", parent: "up.engines.node.SNode"=None):
+    def __init__(self, action: "up.engines.action.Action", stn: "up.plans.stn.STNPlan", parent: "up.engines.node.SNode"=None):
         super().__init__()
         self._action = action
         self._parent = parent
         self._children: Dict["up.engines.State","up.engines.node.SNode"] = {}
-        self._stn = None #TODO: add
+        self._stn = stn
+
+        self._add_constraints()
 
     def __repr__(self):
         s = "action Node; children: %d; visits: %d; reward: %f" % (len(self.children), self.count, self.value)
@@ -98,6 +101,30 @@ class ANode(Node):
 
     def isLeaf(self):
         return self.children
+
+    @property
+    def stn(self):
+        return self._stn
+
+    def _add_constraints(self):
+        previous_action = self.parent.parent
+        previous_node = None
+
+        if previous_action:
+            if isinstance(previous_action, up.engines.action.InstantaneousEndAction):
+                previous_node = up.plans.stn.STNPlanNode(up.model.timing.TimepointKind.END, up.plans.stn.STNPlanNode(previous_action))
+            else:
+                previous_node = up.plans.stn.STNPlanNode(up.model.timing.TimepointKind.START, up.plans.stn.STNPlanNode(previous_action))
+
+        update_stn(self.stn, self.action, previous_node)
+
+
+
+
+
+
+
+
 
 
 
