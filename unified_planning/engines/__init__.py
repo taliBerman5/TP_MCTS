233a73c969f7d207ef74385a9f0f06c1255d439b
taliberman5 <taliberman5@gmail.com>
2023-08-12 17:55:46 +0300

    added nodes without STN support

--- unified_planning/engines/__init__.py
+++ unified_planning/engines/__init__.py
@@ -13,13 +13,15 @@ from unified_planning.engines.action import (
 from unified_planning.engines.node import (
     ANode,
     SNode,
+    C_ANode,
+    C_SNode,
 )
 from unified_planning.engines.state import State, CombinationState, ActionQueue, QueueNode
 from unified_planning.engines.mdp import MDP, combinationMDP
 from unified_planning.engines.mixins.compiler import CompilationKind
 from unified_planning.engines.engine import Engine
 from unified_planning.engines.compilers.grounder import Grounder, GrounderHelper
-from unified_planning.engines.solvers.mcts import (plan, MCTS)
+from unified_planning.engines.solvers.mcts import (plan, C_MCTS)
 from unified_planning.engines.utils import create_init_stn, update_stn
 
 
@@ -35,6 +37,8 @@ __all__ = [
     "NoOpAction",
     "ANode",
     "SNode",
+    "C_ANode",
+    "C_SNode",
     "State",
     "CombinationState",
     "ActionQueue",
@@ -45,7 +49,7 @@ __all__ = [
     "Engine",
     "Grounder",
     "GrounderHelper",
-    "MCTS",
+    "C_MCTS",
     "plan",
     "create_init_stn",
     "update_stn",
