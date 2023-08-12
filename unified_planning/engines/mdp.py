87db7d8cfb7cd592394dd2a7fc1d205b7c843ffe
taliberman5 <taliberman5@gmail.com>
2023-08-12 12:21:34 +0300

    added combinationMDP

--- unified_planning/engines/mdp.py
+++ unified_planning/engines/mdp.py
@@ -56,26 +56,22 @@ class MDP:
 
         return legal_actions
 
-    def step(self, state: "up.engines.State", action: "up.engines.action.Action"):
-        """
-               Apply the action to this state to produce the next state.
-        """
-        # # Make sure it is legal to run the action in the state
-        # if not action.pos_preconditions.issubset(state.predicates):
-        #     msg = f"Some of the positive preconditions of action {action.name} dont hold is the current state."
-        #     raise UPPreconditionDonHoldException(msg)
-        #
-        # if not action.neg_preconditions.isdisjoint(state.predicates):
-        #     msg = f"Some of the negative preconditions of action {action.name} dont hold is the current state."
-        #     raise UPPreconditionDonHoldException(msg)
-
-        new_preds = set(state.predicates)
+    def update_predicate(self, state: "up.engines.State", new_preds: set, action: "up.engines.action.Action"):
         new_preds |= action.add_effects
         new_preds -= action.del_effects
 
         add_predicates, del_predicates = self._apply_probabilistic_effects(state, action)
         new_preds |= add_predicates
         new_preds -= del_predicates
+
+        return new_preds
+
+    def step(self, state: "up.engines.State", action: "up.engines.action.Action"):
+        """
+               Apply the action to this state to produce the next state.
+        """
+        new_preds = set(state.predicates)
+        new_preds = self.update_predicate(state, new_preds, action)
         next_state = up.engines.State(new_preds)
 
         terminal = self.is_terminal(next_state)
@@ -107,3 +103,83 @@ class MDP:
                         del_predicates.add(v)
 
         return add_predicates, del_predicates
+
+
+class combinationMDP(MDP):
+    def __init__(self, problem: "up.model.problem.Preoblem", discount_factor: float):
+        super().__init__(problem, discount_factor)
+
+    def initial_state(self):
+        """
+
+        :return: the initial state of the problem
+        """
+        predicates = self.problem.initial_values
+        pos_predicates = set([key for key, value in predicates.items() if value.bool_constant_value()])
+        return up.engines.CombinationState(pos_predicates)
+
+    def is_terminal(self, state: "up.engines.state.CombinationState"):
+        """
+        Checks if all the goal predicates hold in the `state`
+        and there are no active actions
+
+        :param state: checked state
+        :return: True is the `state` is a terminal state, False otherwise
+        """
+        return super().is_terminal(state) and not state.is_active_actions
+
+    def step(self, state: "up.engines.CombinationState", action: "up.engines.action.Action"):
+        """
+               Apply the action to this state to produce the next state.
+
+               If the action is:
+
+               - Instantaneous: the predicates are updated according to the action effects
+
+               - Durative: adds the action to the active action and adds inExecution predicate
+               - Combination: adds each of the durative actions to the active action and add inExecution predicates
+               - No-op: nothing is added.
+
+               for each Durative, Combination, No-op:
+                   Finds the action(s) with the shortest duration left,
+                   updates the predicates according to this action(s)
+                   extracts delta from the rest of the active actions
+
+        """
+
+        new_preds = set(state.predicates)
+        new_active_actions = state.active_actions.clone()
+
+        if isinstance(action, up.engines.InstantaneousAction):
+            new_preds = super().update_predicate(state, new_preds, action)
+            next_state = up.engines.CombinationState(new_preds, new_active_actions)
+
+        # Deals with no-op, durative actions and combination actions
+        else:
+            if isinstance(action, up.engines.DurativeAction):
+                new_active_actions.add_action(up.engines.QueueNode(action, action.duration.lower.int_constant_value()))
+                new_preds |= action.inExecution
+
+            elif isinstance(action, up.engines.CombinationAction):
+                for a in action.actions:
+                    new_active_actions.add_action(up.engines.QueueNode(a, a.duration.lower.int_constant_value()))
+
+                new_preds |= action.inExecution
+
+            delta, actions_to_perform = new_active_actions.get_next_actions()
+
+            for a in actions_to_perform:
+                new_preds = super().update_predicate(state, new_preds, a)
+
+            if delta != -1:
+                new_active_actions.update_delta(delta)
+
+            next_state = up.engines.CombinationState(new_preds, new_active_actions)
+
+        terminal = self.is_terminal(next_state)
+
+        # common = len(self.problem.goals.intersection(state.predicates))
+        # reward = 100 if terminal else 2 ** (common - len(self.problem.goals))
+        reward = 10 if terminal else 0
+
+        return terminal, next_state, reward
