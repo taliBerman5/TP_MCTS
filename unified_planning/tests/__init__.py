6a8625535ec73c694dee382a4a6ec07b5d5b3a75
taliberman5 <taliberman5@gmail.com>
2023-08-12 12:18:29 +0300

    added problem

--- unified_planning/tests/__init__.py
+++ unified_planning/tests/__init__.py
@@ -1,7 +1,10 @@
-from unified_planning.tests.problems import mutex_converted_problem, OAP_converted_problem
+from unified_planning.tests.problems import (mutex_converted_problem,
+                                             OAP_converted_problem,
+                                             combination_converted_problem)
 
 
 __all__ = [
     "mutex_converted_problem",
-    "OAP_converted_problem"
+    "OAP_converted_problem",
+    "combination_converted_problem",
     ]
