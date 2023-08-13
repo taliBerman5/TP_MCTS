164732a9f959f0cb19ec9b92817e85ae1e56ce01
taliberman5 <taliberman5@gmail.com>
2023-08-13 17:05:26 +0300

    update logic

--- unified_planning/engines/solvers/rtdp.py
+++ unified_planning/engines/solvers/rtdp.py
@@ -9,13 +9,14 @@ from unified_planning.engines.utils import (
 from collections import defaultdict
 
 random.seed(10)
+
+
 class RTDP:
     def __init__(self, mdp, root_state: "up.engines.state.State", search_depth: int):
         self._mdp = mdp
         self._root_state = root_state
         self._search_depth = search_depth
-        self.V = defaultdict(lambda: 0.) # TODO:if the heuristic is not 0 these needs to ne changed to {}
-
+        self.V = defaultdict(lambda: 0.)  # TODO:if the heuristic is not 0 these needs to ne changed to {}
 
     @property
     def mdp(self):
@@ -29,6 +30,18 @@ class RTDP:
     def search_depth(self):
         return self._search_depth
 
+    def search(self, timeout):
+        start_time = time.time()
+        current_time = time.time()
+        i = 0
+        while current_time < start_time + timeout:
+            self.trial()
+            current_time = time.time()
+            i += 1
+        print(f'i = {i}')
+
+        best_action, _ = self.evaluate(self.root_state)
+        return best_action
 
     def trial(self):
         """
@@ -36,18 +49,23 @@ class RTDP:
         state = self.root_state
         terminal = False
 
-        while not terminal:
+        while not terminal:  # TODO: add another stopping criteria (number of steps or time)
             action, action_value = self.evaluate(state)
             self.V[state] = action_value
             terminal, state, reward = self.mdp.step(state, action)
-
 
     def eval_action(self, state: "up.engines.State", action: "up.engines.Action"):
         # Heuristic value if the state is not stored in V table else V[state]
 
         _, _, reward = self.mdp.step(state, action)
-        Q_s_a = reward + self.mdp.discount_factor # TODO: multiply be sum of probability function multiply by V or H
+        trans = self.mdp.transition_function(state, action)
+        nextV = 0
+        for state, prob in trans:
+            nextV += prob * self.V[state]
 
+        Q_s_a = reward + self.mdp.discount_factor * nextV   # TODO: multiply be sum of probability function multiply by V or H
+        if Q_s_a > 0:
+            4
         return Q_s_a
 
     def evaluate(self, state: "up.engines.State"):
@@ -61,58 +79,28 @@ class RTDP:
         return best_a, best_value
 
 
-
-
-
-
-
-    def best_action(self, root_node: "up.engines.SNode"):
-        """
-
-        :param root_node: the root node of the MCTS tree
-        :return: returns the best action for the `root_node`
-        """
-        anodes = root_node.children
-        aStart_value = float("-inf")
-        aStar = -1
-
-        for action in root_node.possible_actions:
-            if anodes[action].value > aStart_value:
-                aStart_value = anodes[action].value
-                aStar = action
-
-        return aStar
-
-
-def plan(mdp: "up.engines.MDP", steps: int, search_depth: int, exploration_constant: float):
+def plan(mdp: "up.engines.MDP", steps: int, search_depth: int):
     root_state = mdp.initial_state()
 
-    history = []
+    rtdp = RTDP(mdp, root_state, search_depth)
+    rtdp.search(1)
     step = 0
-    # for i in range(steps):
+    history = []
+
     while True:
         print(f"started step {step}")
-        mcts = RTDP(mdp, root_state, search_depth)
-        action = mcts.search(1)
+        rtdp = RTDP(mdp, root_state, search_depth)
+        action = rtdp.search(1)
 
-        if action == -1:
-            print("A valid plan is not found")
-            break
+        terminal, root_state, reward = mdp.step(root_state, action)
 
         print(f"Current state is {root_state}")
         print(f"The chosen action is {action.name}")
 
-
-
-        terminal, root_state, reward = mcts.mdp.step(root_state, action)
-
-
-        # previous_STNNode = history[-1] if history else None
         history.append(action)
-
 
         if terminal:
             print(f"Current state is {root_state}")
             break
 
-        step +=1
+        step += 1
