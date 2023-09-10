43354b48a790cfc90c447dfeb58710e78335a328
taliberman5 <taliberman5@gmail.com>
2023-09-10 18:28:27 +0300

    changed the argument

--- unified_planning/parser.py
+++ unified_planning/parser.py
@@ -6,7 +6,7 @@ parser.add_argument('-st', '--search_time', help='amount of time in each step', 
 parser.add_argument('-sd', '--search_depth', help='search depth of ', nargs='?', default=40, type=int)
 parser.add_argument('-se', '--selection_type', help='selection type in MCTS algorithm', nargs='?', default='avg')
 parser.add_argument('-r', '--runs', help='how many runs to run the script', nargs='?', default=1, type=int)
-parser.add_argument('-b', '--baseline', help='baseline or new approach',  action='store_true')
+parser.add_argument('-dt', '--domain_type', help='combination, new approach or new approach same as the baseline', nargs='?', default='regular')
 parser.add_argument('-s', '--solver', help='solver', nargs='?', default='mcts')
 parser.add_argument('-do', '--domain', help='domain')
 parser.add_argument('-e', '--exploration_constant', help='the exploration constant for mcts solver', nargs='?', default=10, type=int)
