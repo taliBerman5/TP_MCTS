9bb02fb3b6d5c29b2227d4cd1a9969423927f9cf
taliberman5 <taliberman5@gmail.com>
2023-09-18 13:09:53 +0300

    new domain

--- unified_planning/domains/__init__.py
+++ unified_planning/domains/__init__.py
@@ -2,6 +2,7 @@ from unified_planning.domains.domain import Domain
 from unified_planning.domains.machine_shop import Machine_Shop
 from unified_planning.domains.nasa_rover import Nasa_Rover
 from unified_planning.domains.stuck_car import Stuck_Car
+from unified_planning.domains.strips import Strips
 
 
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     "Machine_Shop",
     "Nasa_Rover",
     "Stuck_Car",
+    "Strips",
     ]
